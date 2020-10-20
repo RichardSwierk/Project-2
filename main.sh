15ad0fba77c6b391b105aaeb4e1f1cd9fb1c6340
Richard T Swierk <richard.swierk@mymail.champlain.edu>
2020-10-20 13:07:56 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,12 +2,23 @@
 
 emails=(rswierk10@gmail.com richardswierk8@gmail.com rtswierk@gmail.com)
 
+#Loop through all of the emails in the list
 for x in "${emails[@]}"
 do
   email=$x
-  username=
+  #Get everything before the @
+  username=${x%@*}
+  #Create temp random password with a length of 12
+  tempPass="$(openssl rand -base64 12)"
+  #Create user with a home directory, add the user to group CSI230, set the users password to the random password created above
+  useradd -m -g CSI230 -p $tempPass $username
+  #Makes the user change there password after the first time they login
+  chage -d 0 $username
+  #Create the email
+  echo -e 'Subject: Your temp account password\nPassword must be changed after first login\nUsername: $username\nPassword: $tempPass' > email.txt
+  #Send the email
+  sendmail $email < email.txt
 done
 
 
-echo 'Subject: Your temp account password\nPassword: $tempPass' > email.txt
-sendmail something@gmail.com < email.txt
+
